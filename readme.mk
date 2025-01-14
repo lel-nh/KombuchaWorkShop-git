feb2947fb51fb46a2d4b237451ebcb1c956e63f5
lel <108673271+lel-nh@users.noreply.github.com>
2025-01-14 11:43:42 +0100

    readme

---
+++ readme.mk
@@ -0,0 +1,11 @@
+# Welcome here !
+
+Welcome to the Kombucha Workshop project! This repository contains all the necessary information and resources for the Kombucha Workshop.
+
+## Documentation
+
+For detailed information about the project, please refer to the [KombuchaWorkshop.pdf](./KombuchaWorkshop.pdf).
+
+We hope you find everything you need here. If you have any questions, feel free to reach out.
+
+Happy brewing!
